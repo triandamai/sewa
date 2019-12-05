a5f4df8e66ae12cb6fdc10fdcb981689cb2b174c
Trian Damai <triannurizkillah@gmail.com>
2019-12-05 19:49:30 +0700

    pembuatan projek

---
+++ app/src/main/java/com/pmo/sewa/pages/penyedia/base_page.java
@@ -0,0 +1,16 @@
+package com.pmo.sewa.pages.penyedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.pmo.sewa.R;
+
+public class base_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base_page);
+    }
+}
